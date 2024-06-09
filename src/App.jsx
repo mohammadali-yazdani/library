036b93be39163a1c38065035de2c7b523ae4f49b
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2024-06-09 13:37:01 +0300

    Feat: Insert All components in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Books } from "./components/Books";
+import { Header } from "./components/Header";
+import { SearchBar } from "./components/SearchBar";
+
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <Header />
+      <main className="container max-w-4xl px-4 py-10 mx-auto">
+        <SearchBar />
+        <Books />
+      </main>
+    </div>
+  );
 }
 
 export default App;
